cad055ed7e3d642637bfaf74b4ebf8a9cd056326
nahid-linux-desktop <b.m.shamsnahid@gmail.com>
2021-05-13 15:55:21 +0600

    Using the body mapping tempalte with serverless

--- resource/serverless/hello-serverless/add.js
+++ resource/serverless/hello-serverless/add.js
@@ -2,13 +2,19 @@
 
 module.exports.add = async (event) => {
   const { num1, num2 } = JSON.parse(event.body);
+  const output = {
+    num1,
+    num2,
+    result: num1 + num2,
+  };
+
+  console.log(output);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        num1,
-        num2,
-        result: num1 + num2,
+        ...output,
         event
       },
       null,
